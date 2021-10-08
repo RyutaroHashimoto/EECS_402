8d13b5524037709183fcc97536ab7e2fe0969c07
ryutaro <h.ryuutarou1234@gmail.com>
2021-10-08 18:39:04 -0400

    add code

---
+++ project2/project2.cpp
@@ -0,0 +1,15 @@
+//library
+#include <iostream>
+using namespace std;
+
+//program header
+//Name: Ryutaro Hashimoto
+//Date: October 12, 2021
+//purpose: This program will define color image class. Image is composed of a 
+//collection of pixels. Colors re described by RGB values.
+
+//global constant
+
+
+//global function
+//Function: Call accrueOneMonthsInterest for numMonths times.
